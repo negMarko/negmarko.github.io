1be5e751bd6893cc2f2c07d4f4ad7ca57aaca668
negMarko <negus.marko@gmail.com>
2018-02-07 11:41:05 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
-<!-- Aqui ira codigo  -->
+ // Add the addAll method
+  importScripts('sw-cache-addall.js');
+ 
+  // A list of paths to cache
+  var paths = [
+    '/',
+    '/css/main.css',
+    '/js/main.js',
+    'index.htm'
+  ];
+ 
+  // Open the cache (and name it)
+  caches.open('offline-v1').then(function(cache) {
+    return cache.addAll(paths);
+  })
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+navigator.serviceWorker.controller.postMessage({'command': 'say-hello!'})
